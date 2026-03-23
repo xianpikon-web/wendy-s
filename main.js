d15ef13d1c6ec3b386618de42d7ee44343f6c573
xianpikon-web <xianpikon@gmail.com>
2026-03-23 18:30:56 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -282,3 +282,16 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+const reveals = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+    reveals.forEach(el => {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            el.style.opacity = "1";
+            el.style.transform = "translateY(0)";
+        }
+    });
+});
